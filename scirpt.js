39070bfde54cbdb0a7cc23594218ae9091745846
Tertili4428 <yesidvells@gmail.com>
2025-05-14 09:19:06 -0700

    Create scirpt.js

---
+++ scirpt.js
@@ -0,0 +1,7 @@
+// Capturar el envío del formulario de PayPal
+const paypalForm = document.getElementById('paypal_form');
+
+paypalForm.addEventListener('submit', function(event) {
+  // Mostrar mensaje de confirmación
+  alert("¡Compra en proceso! Gracias por confiar en Amor de Madre 💖");
+});
